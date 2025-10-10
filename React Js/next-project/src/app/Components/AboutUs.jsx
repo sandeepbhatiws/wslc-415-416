91a6c8b7a5bd6c3247188b9447997ea0d5aaa833
Sandeep Bhati <sandeep@gmail.com>
2025-10-10 22:19:17 +0530

    Changes

---
+++ React Js/next-project/src/app/Components/AboutUs.jsx
@@ -0,0 +1,12 @@
+"use client";
+import React, { useState } from 'react'
+
+export default function AboutUs() {
+    const [variable, setVariable] = useState('');
+
+  return (
+    <>
+      About Us
+    </>
+  )
+}
---
+++ React Js/next-project/src/app/page.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import Home from "./Components/Home";
+
+export default function page() {
+  return (
+    <Home/>
+  );
+}
